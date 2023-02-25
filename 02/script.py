94ef881463d2fa7965ec56c63a837ce6cacf7ca3
Daniel Storer <danielstorer54@gmail.com>
2023-02-25 11:05:59 +0000

    Completed up to and including week 4

---
+++ 02/script.py
@@ -0,0 +1,3 @@
+for i in range(0,16):
+    j = i+1
+    print(f"bit(in=in[{i}], load=load, out=w{j});")
---
+++ 04/fill/Fill.asm
@@ -0,0 +1,42 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Fill.asm
+
+// Runs an infinite loop that listens to the keyboard input.
+// When a key is pressed (any key), the program blackens the screen,
+// i.e. writes "black" in every pixel;
+// the screen should remain fully black as long as the key is pressed. 
+// When no key is pressed, the program clears the screen, i.e. writes
+// "white" in every pixel;
+// the screen should remain fully clear as long as no key is pressed.
+
+// Put your code here.
+    @n
+    M=0
+(LOOP)
+    @n
+    D=M
+    @R1
+    D=D-M
+    @END
+    D;JEQ
+
+    @R0
+    D=M
+    @n
+    A=D+M
+    M=-1
+
+    @n
+    M=M+1
+
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
+    
+// if keycode is 0 display white screen
+
+// if keycode is not 0 display black screen
